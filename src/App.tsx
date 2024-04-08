0bdecc7852d98e8581925b392d3192be0cc86556
Maxim Chizhevskiy <max15.91@mail.ru>
2024-04-08 23:01:16 +0500

    add Controled Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import Rating, {RatingValueType} from "./components/Rating/Rating";
+import OnOff from "./components/OnOff/OnOff";
+import UncontrolledAccordion from "./components/UncontrolledAccordion/UncontrolledAccordion";
+import UncontrolledRating from "./components/UncontrolledRating/UncontrolledRating";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log("App rendered")
+    const [ratingValue, setRatingValue] = useState<RatingValueType>(3)
+    const [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false)
+    return (
+        <div className="App">
+            <AppTitle/>
+            <Rating value={ratingValue} onClick={setRatingValue}/>
+            <Accordion titleValue={"MyMenu"}
+                       setAccordionCollapsed={setAccordionCollapsed}
+                       collapsed={accordionCollapsed}
+                       itemOneValue={"Item1"}
+                       itemTwoValue={"Item2"}
+                       itemThreeValue={"Item3"} />
+
+            {/*<UncontrolledAccordion titleValue={"Menu"}
+                                   itemOneValue={"Item1"}
+                                   itemTwoValue={"Item2"}
+                                   itemThreeValue={"Item3"}
+            />
+            <UncontrolledAccordion titleValue={"Users"}
+                                   itemOneValue={"Микки"}
+                                   itemTwoValue={"Дональд"}
+                                   itemThreeValue={"Скрудж"}
+            />
+            <UncontrolledRating/>
+            <OnOff />
+            <OnOff />*/}
+            <UncontrolledRating/>
+        </div>
+    );
+}
+
+function AppTitle() {
+    console.log("AppTitle rendered")
+    return (
+        <>
+            Welcome to IT-INCUBATOR
+        </>
+    );
 }
 
 export default App;
