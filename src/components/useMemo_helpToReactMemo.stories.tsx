38b2d4297a972370872d9397254947f29aef1ea6
MaximChizhevskiy <max15.91@mail.ru>
2024-10-04 23:02:57 +0500

    add useCallback

--- src/components/useMemo_helpToReactMemo.stories.tsx
+++ src/components/useMemo_helpToReactMemo.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from "react";
+import React, {useCallback, useMemo, useState} from "react";
 
 export default {
     title: 'useMemo for React.memo'
@@ -22,7 +22,7 @@ export const MainComponent = () => {
     const [users, setUsers] = useState<string[]>(['Max', 'Daulet', 'Dauren'])
 
     const newArray = useMemo(() => {
-       return  users.filter((u)=>u.toLowerCase().indexOf('e') > -1)
+        return users.filter((u) => u.toLowerCase().indexOf('e') > -1)
     }, [users]);
 
     const addUser = () => {
@@ -38,3 +38,46 @@ export const MainComponent = () => {
         <UsersInMemo users={newArray}/>
     </div>
 }
+//========================================================================
+//========================================================================
+
+export const LikeUseCallback = () => {
+    console.log('LikeUseCallback')
+    const [counter, setCounter] = useState<number>(0)
+    const [books, setBooks] = useState<string[]>(['React', 'Redux', 'TypeScript'])
+
+
+    /*const memoizedAddBook = useMemo(()=> {
+        return () => {
+            console.log('addBook')
+            setBooks([...books, 'Vue'])
+        }
+    }, [books])*/
+
+    const memoizedAddBook = useCallback(()=> {
+            console.log('addBook')
+            setBooks([...books, 'Vue'])
+    }, [books])
+
+    return <div>
+        <button onClick={() => {setCounter(counter + 1)}}>+</button>
+        {counter}
+        <BooksInMemo addBook={memoizedAddBook}/>
+    </div>
+}
+
+
+type BooksListPropsType = {
+    //books: Array<string>,
+    addBook: () => void
+}
+
+const BooksList = (props: BooksListPropsType) => {
+    console.log("Books rendered")
+    return <div>
+        <button onClick={props.addBook}>Add</button>
+        {/*{props.books.map((b, i) => <div key={i}>{b}</div>)}*/}
+    </div>
+}
+
+const BooksInMemo = React.memo(BooksList)
