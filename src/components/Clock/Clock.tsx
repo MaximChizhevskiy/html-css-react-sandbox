965475e578383844d208c730707706d9aeb10faf
MaximChizhevskiy <max15.91@mail.ru>
2024-12-05 23:49:21 +0500

    add cleanup for useEffect

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -37,3 +37,4 @@ export type ClockViewPropsType = {
     date: Date
 }
 
+
--- src/components/UseEffect.stories.tsx
+++ src/components/UseEffect.stories.tsx
@@ -35,3 +35,45 @@ export const MainComponent = () => {
         </>
     )
 }
+
+export const KeyTrackerExample = () => {
+    const [text, setText] = useState('')
+    console.log('Component rendered with' + text)
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            console.log(e.key)
+            setText(text + e.key)
+        }
+
+        window.addEventListener('keypress', handler)
+
+        return () => {
+            window.removeEventListener('keypress', handler)
+        }
+    }, [text]);
+
+    return <>
+        Typed text : {text}
+    </>
+}
+
+export const setTimeOutExample = () => {
+    const [text, setText] = useState('')
+    console.log('Component rendered with ' + text)
+
+    useEffect(() => {
+        const timeOutId = setTimeout(() => {
+            console.log('TIMEOUT EXPIRED')
+            setText('3 seconds passed')
+        }, 3000)
+
+        return () => {
+            clearTimeout(timeOutId)
+        }
+    }, []);
+
+    return <>
+        text: {text}
+    </>
+}
